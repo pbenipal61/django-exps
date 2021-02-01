d6604f4eb972de052a289ed1472bd92e85a7c3fb
Singh, Prabhjot <Prabhjot.Singh@F-Secure.com>
2021-02-01 16:19:50 +0200

    adopted generic mixins

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,28 +1,14 @@
-from rest_framework import mixins, generics
+from rest_framework import generics
 from .models import Post
 from .serializers import PostSerializer
 
 
-class PostList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class PostDetails(generics.GenericAPIView, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+class PostDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(self, request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(self, request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(self, request, *args, **kwargs)
