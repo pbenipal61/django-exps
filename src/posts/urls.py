8db2ae89f7f22969d32bdeb559bcc607dcc547d8
Singh, Prabhjot <Prabhjot.Singh@F-Secure.com>
2021-02-01 16:50:02 +0200

    added hyperlinked views

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import PostList, PostDetails, UserDetails, UserList
+from .views import PostList, PostDetails, UserDetails, UserList, api_root
 from rest_framework.urlpatterns import format_suffix_patterns
 
 
 urlpatterns = format_suffix_patterns([
-    path('posts/', PostList.as_view()),
-    path('posts/<int:pk>/', PostDetails.as_view()),
-    path('users/', UserList.as_view()),
-    path('users/<int:pk>/', UserDetails.as_view())
+    path('', api_root),
+    path('posts/', PostList.as_view(), name='post-list'),
+    path('posts/<int:pk>/', PostDetails.as_view(), name='post-detail'),
+    path('users/', UserList.as_view(), name='user-list'),
+    path('users/<int:pk>/', UserDetails.as_view(), name='user-detail')
 ])
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,9 +1,20 @@
 from rest_framework import generics
 from rest_framework import permissions
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from django.contrib.auth.models import User
 from .models import Post
 from .serializers import PostSerializer, UserSerializer
 from .permissions import IsOwnerOrReadOnly
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'posts': reverse('post-list', request=request, format=format)
+    })
 
 
 class UserList(generics.ListAPIView):
