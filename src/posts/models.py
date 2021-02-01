ebf0265d1fde0c24a27a524994b7804a393b868f
Singh, Prabhjot <Prabhjot.Singh@F-Secure.com>
2021-02-01 11:31:12 +0200

    initial commit

---
+++ src/posts/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Post(models.Model):
+    created = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=100, blank=False)
+    content = models.TextField()
+    author = models.CharField(max_length=100, blank=False)
+
+    class Meta:
+        ordering = ['created']
---
+++ src/posts/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import Post
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'content', 'author']
+
+    def create(self, validated_data):
+        return Post.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.content = validated_data.get('content', instance.content)
+        instance.author = validated_data.get('author', instance.author)
+        instance.save()
+        return instance
+
